8a75a75a4cb397e52da60f8065572ae7c699ecc9
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2025-01-03 11:48:27 +0900

    setcion 04 2,3번 문제풀이 업데이트

--- section04/02.js
+++ section04/02.js
@@ -1,7 +1,35 @@
 // Title: 뒤집은 소수
+// 스트링 변환후 쪼개서 뒤집고 다시 붙이고 -> number로 변경 ( 0 삭제됨 ) -> isPrime 함수로 소수 판별
+
+function isPrime(num) {
+    if (num === 1) return false;
+    for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+        if (num % i === 0) return false;
+    }
+
+    return true;
+}
 
 function solution(arr) {
     let answer = [];
+
+    for (let x of arr) {
+        let res = Number(x.toString().split('').reverse().join(''));
+        if (isPrime(res)) answer.push(res);
+    }
+
+    /* Soution02
+    for (x of arr) {
+        let res = 0;
+
+        while (x) {
+            let t = x % 10;
+            res = res * 10 + t;
+            x = parseInt(x / 10);
+        }
+
+        if (isPrime(res)) answer.push(res);
+    } */
 
     return answer;
 }
---
+++ section04/03.js
@@ -0,0 +1,42 @@
+function solution(test) {
+    let answer = 0,
+        tmp = [];
+    let m = test.length; // 테스트 횟수
+    let n = test[0].length; // 학생 수
+
+    for (let i = 1; i <= n; i++) {
+        // 멘토 후보
+        for (let j = 1; j <= n; j++) {
+            // 멘티 후보
+            let cnt = 0; // i가 j보다 등수가 높은 테스트 수 초기화
+            // 각 테스트에서 등수 비교 시작
+            for (let k = 0; k < m; k++) {
+                // i와 j의 등수
+                let pi = 0,
+                    pj = 0;
+                for (let s = 0; s < n; s++) {
+                    // 등수 확인
+                    if (test[k][s] === i) pi = s;
+                    if (test[k][s] === j) pj = s;
+                }
+                if (pi < pj) cnt++; // i가 j보다 등수가 높으면 카운트
+            }
+
+            if (cnt === m) {
+                // 모든 테스트에서 i가 j보다 높을 때
+                tmp.push([i, j]);
+                answer++; // 카운트 증가
+            }
+        }
+    }
+
+    console.log(tmp);
+    return answer; // 가능한 쌍의 총 개수 반환
+}
+
+let arr = [
+    [3, 4, 1, 2],
+    [4, 3, 2, 1],
+    [3, 1, 4, 2],
+];
+console.log(solution(arr));
