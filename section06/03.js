1c5591c3fe600cf9436a31891da94a793f97841e
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2025-01-22 10:57:18 +0900

    fix: setcion06 3번 문제풀이 수정

--- section06/03.js
+++ section06/03.js
@@ -1,3 +1,4 @@
+// Title: 크레인 인형뽑기(카카오 기출)
 function solution(board, moves) {
     let answer = 0; // 점수를 저장할 변수
     let stack = []; // 인형을 쌓을 스택
@@ -30,3 +31,14 @@ function solution(board, moves) {
 
     return answer;
 }
+
+let a = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
