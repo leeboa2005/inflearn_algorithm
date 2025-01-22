277cc486b7ebd98c488650dfbb9d740b0801f8f4
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2025-01-22 10:54:16 +0900

    update: setcion06 3번 문제풀이 주석 추가

--- section06/03.js
+++ section06/03.js
@@ -1,17 +1,28 @@
 function solution(board, moves) {
-    let answer = 0;
-    let stack = [];
+    let answer = 0; // 점수를 저장할 변수
+    let stack = []; // 인형을 쌓을 스택
 
+    // moves 배열을 순회
     moves.forEach((pos) => {
+        // 각 열에 대해 인형을 찾기 위해 반복
         for (let i = 0; i < board.length; i++) {
+            // 현재 열에서 인형이 있는지 확인
             if (board[i][pos - 1] !== 0) {
-                tmp = board[i][pos - 1];
+                // 인형을 tmp 변수에 저장
+                let tmp = board[i][pos - 1];
+                // 보드에서 해당 인형을 제거
                 board[i][pos - 1] = 0;
 
+                // 스택의 마지막 인형과 현재 인형이 같은지 확인
                 if (tmp === stack[stack.length - 1]) {
+                    // 같다면 스택에서 제거하고 점수 2점 추가
                     stack.pop();
                     answer += 2;
-                } else stack.push(tmp);
+                } else {
+                    // 다르다면 스택에 추가
+                    stack.push(tmp);
+                }
+                // 인형을 찾았으므로 반복 종료
                 break;
             }
         }
@@ -19,14 +30,3 @@ function solution(board, moves) {
 
     return answer;
 }
-
-let a = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 3],
-    [0, 2, 5, 0, 1],
-    [4, 2, 4, 4, 2],
-    [3, 5, 1, 3, 1],
-];
-
-let b = [1, 5, 3, 5, 1, 2, 1, 4];
-console.log(solution(a, b));
