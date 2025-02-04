7030dace9fb915ecbe461cd9be370bb576681fcb
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2025-02-04 12:29:04 +0900

    section07 5번 문제풀이 업데이트

--- section07/05.js
+++ section07/05.js
@@ -1,5 +1,26 @@
 // Title: LRU
 function solution(size, arr) {
+    // let answer = Array.from({ length: size }, () => 0);
+    let answer = [];
+    arr.forEach((x) => {
+        let pos = -1;
+        for (let i = 0; i < size; i++) if (x === answer[i]) pos = i;
+        if (pos === -1) {
+            // for (let i = size - 1; i >= 1; i--) {
+            //     answer[i] = answer[i - 1];
+            // }
+            answer.unshift(x);
+            if (answer.length > size) answer.pop();
+        } else {
+            // for (let i = pos; i >= 1; i--) {
+            //     answer[i] = answer[i - 1];
+            // }
+            answer.splice(pos, 1);
+            answer.unshift(x);
+        }
+        answer[0] = x;
+    });
+
     return answer;
 }
 
